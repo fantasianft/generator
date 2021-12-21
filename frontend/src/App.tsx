b2565424c3dae259ef6079d316705a56e9b3887a
Diogo Costa <diogo.costa@lendingworks.co.uk>
2021-12-21 18:36:53 +0000

    NFT-30: create React app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Generate</Button>
+    </div>
+  );
+}
+
+export default App;
