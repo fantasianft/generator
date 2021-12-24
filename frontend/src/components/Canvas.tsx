072e390c966ff40f1cd7eb52fb56175a0a26051c
Diogo Costa <hello@diogothecoder.com>
2021-12-24 21:53:33 +0000

    NFT-30: removed old commented out code

--- frontend/src/components/Canvas.tsx
+++ frontend/src/components/Canvas.tsx
@@ -1,10 +1,7 @@
-import axios from "axios";
-import React, { Component, useContext, useEffect } from "react";
-import * as THREE from "three";
+import { Component, useContext, useEffect } from "react";
 import { Context } from "../Store";
 import { getSelectedLayers } from "./DropdownList";
 import Sketch from "react-p5";
-import { Image, loadImage } from "canvas";
 
 export const Canvas = () => {
   const [state, setState] = useContext(Context);
@@ -17,29 +14,25 @@ export const Canvas = () => {
 };
 
 class CanvasComponent extends Component<any, any> {
-  // "/layers/Body/body%231.png"
-
-  imageUrls = ["layers/Body/body%231.png", "layers/Face/face%231.png"];
-  loadedImages = [];
-
   images: any = [];
-  img: any;
 
-  setup = (p5: any, parentRef: any) => {
-    p5.createCanvas(1000, 1000).parent(parentRef);
+  preload = (p5: any) => {
     getSelectedLayers(this.props.layers).then((res: any) => {
       const layers = res.data;
       layers.forEach((layer: any) => {
         const imageUrl = `layers/${layer.name}/${layer.selectedElement.filename}`;
-        p5.loadImage(encodeURIComponent(imageUrl), (img: any) => {
-          p5.image(img, 0, 0);
-        });
+        this.images.push(p5.loadImage(encodeURIComponent(imageUrl)));
       });
-      console.log(layers);
     });
   };
 
-  draw = (p5: any) => {};
+  setup = (p5: any, parentRef: any) => {
+    p5.createCanvas(1000, 1000).parent(parentRef);
+  };
+
+  draw = (p5: any) => {
+    this.images.forEach((imageLoaded: any) => p5.image(imageLoaded, 0, 0));
+  };
 
   componentDidUpdate() {
     console.log("componentDidUpdate");
@@ -48,6 +41,8 @@ class CanvasComponent extends Component<any, any> {
   render() {
     console.log("Render CanvasComponent");
 
-    return <Sketch setup={this.setup} draw={this.draw} />;
+    return (
+      <Sketch preload={this.preload} setup={this.setup} draw={this.draw} />
+    );
   }
 }
